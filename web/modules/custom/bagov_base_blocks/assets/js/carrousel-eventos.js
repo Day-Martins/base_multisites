ca6f6b0fe452746162c98920f35adec152bb8688
Murilo Montino <murilomontinojr@hotmail.com>
2023-12-27 12:05:16 -0300

    Add Drupal DS module and update CSS and block weights
    Add Drupal DS module to composer.json
    Update CSS for carrousel-de-eventos block
    Update block weight for noticias-noticias_home block
    Add we_megamenu_block:account block
    Update CSS for card-carrousel-eventos template
    Add block.block.bagov_base_views_block__carrousel_de_eventos.yml
    Delete block.block.bagov_base_views_block__carrousel_de_eventos_block_1.yml
    Update JS settings for carrousel-eventos.js

--- web/modules/custom/bagov_base_blocks/assets/js/carrousel-eventos.js
+++ web/modules/custom/bagov_base_blocks/assets/js/carrousel-eventos.js
@@ -3,35 +3,37 @@ jQuery(document).ready(function ($) {
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 6,
+    slidesToScroll: 6,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1200,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 5,
+          slidesToScroll: 5,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 576,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
 });
