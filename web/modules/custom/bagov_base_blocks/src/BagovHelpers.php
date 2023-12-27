dd8aee7f98ebc8b1f4ee286740d88c90402c2f90
Gabriel Carneiro <gabriel.carneiro@qintess.com>
2023-12-27 16:37:35 -0300

    feat(noticias): inserção de noticias relacionadas na pagina de noticias e outros ajustes

---
+++ web/modules/custom/bagov_base_blocks/src/BagovHelpers.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\bagov_base_blocks;
+
+use Drupal\Component\Utility\Html;
+use Drupal\views\ViewExecutable;
+
+/**
+ * The primary class for the Ba.Gov Base Blocks module.
+ *
+ * Provides many helper methods.
+ *
+ * @ingroup utility
+ */
+class BagovHelpers {
+  /**
+   * Returns the theme hook definition information.
+   */
+  public static function getThemeHooks() {
+    $hooks = [
+      'contrast_block' => [
+          'render element' => 'content',
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'acesso_informacao_block' => [
+          'render element' => 'content',
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'logo_estado_block' => [
+          'render element' => 'content',
+          'variables' => [
+              'img' => '/sites/default/files/images/logo-governo-rodape.png',
+          ],
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'select_secretaria_block' => [
+          'render element' => 'content',
+          'variables' => [
+              'options' => self::getSecretarias(),
+          ],
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'creative_commons_block' => [
+          'render element' => 'content',
+          'variables' => [
+              'img' => '/sites/default/files/images/creative-commons-nd.png',
+          ],
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'views_noticias' => [
+          'preprocess functions' => [
+            'template_preprocess_views_noticias'
+          ],
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'views_noticias_home' => [
+          'preprocess functions' => [
+            'template_preprocess_views_noticias_home'
+          ],
+          'file' => 'bagov_base_blocks.theme.inc',
+      ],
+      'views_noticias_relacionadas' => [
+        'preprocess functions' => [
+          'template_preprocess_views_noticias_relacionadas'
+        ],
+        'file' => 'bagov_base_blocks.theme.inc',
+    ],
+      'views_view__teste__block_1' => [
+          'base hook' => 'view',
+          'template' => 'template-teste',            
+      ],
+      
+  ];
+
+    return $hooks;
+  }
+
+  /**
+   * Get unique element id.
+   *
+   * @param \Drupal\views\ViewExecutable $view
+   *   A ViewExecutable object.
+   *
+   * @return string
+   *   A unique id for an HTML element.
+   */
+  public static function getUniqueId(ViewExecutable $view) {
+    $id = $view->storage->id() . '-' . $view->current_display;
+    return Html::getUniqueId('views-' . $id);
+  }
+
+  public static function getSecretarias(){
+    $modulePath = \Drupal::service('extension.list.module')->getPath('bagov_base_blocks');
+    return json_decode(file_get_contents($modulePath . '/assets/constants/secretarias.json'));
+  }
+
+}
---
+++ web/modules/custom/bagov_base_blocks/src/Plugin/views/style/ViewsNoticiasRelacionadas.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\bagov_base_blocks\Plugin\views\style;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\style\StylePluginBase;
+
+/**
+ * Style plugin to render each item as a row in a Notícia Principal.
+ *
+ * @ingroup views_style_plugins
+ *
+ * @ViewsStyle(
+ *   id = "views_noticias_relacionadas",
+ *   title = @Translation("Notícias Relacionadas"),
+ *   help = @Translation("Displays rows in Notícias."),
+ *   theme = "views_noticias_relacionadas",
+ *   theme_file = "../bagov_base_blocks.theme.inc",
+ *   display_types = {"normal"}
+ * )
+ */
+class ViewsNoticiasRelacionadas extends StylePluginBase {
+  /**
+   * Does the style plugin for itself support to add fields to it's output.
+   *
+   * @var bool
+   */
+  protected $usesFields = TRUE;
+
+  /**
+   * Definition.
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    // General settings.
+    $options['use_caption'] = ['default' => TRUE];
+
+    // Fields to use in.
+    $options['image'] = ['default' => ''];
+    $options['title'] = ['default' => ''];
+    $options['description'] = ['default' => ''];
+
+    return $options;
+  }
+
+  /**
+   * Render the given style.
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $fields = ['' => $this->t('<None>')];
+    $fields += $this->displayHandler->getFieldLabels(TRUE);
+
+    $form['use_caption'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Add captions to your slides for add title and description over the image.'),
+      '#description' => $this->t('<a href=":docs">See Bootstrap documentation</a>', [':docs' => 'https://getbootstrap.com/docs/4.0/components/carousel/#with-captions']),
+      '#default_value' => $this->options['use_caption'],
+    ];
+
+    $form['image'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Image'),
+      '#options' => $fields,
+      '#default_value' => $this->options['image'],
+    ];
+
+    $form['title'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Title'),
+      '#options' => $fields,
+      '#default_value' => $this->options['title'],
+    ];
+
+    $form['description'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Description'),
+      '#options' => $fields,
+      '#default_value' => $this->options['description'],
+    ];
+  }
+
+}
