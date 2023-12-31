57e8a19c83e57e0be32164e842f7f0de9ee56e67
Murilo Montino <murilomontinojr@hotmail.com>
2023-12-31 19:17:01 -0300

    feat: update styles and functionality for links-rapidos block

--- web/modules/custom/bagov_base_blocks/assets/js/carrousel.js
+++ web/modules/custom/bagov_base_blocks/assets/js/carrousel.js
@@ -41,3 +41,47 @@ jQuery(document).ready(function ($) {
     ]
   });
 });
+
+jQuery(document).ready(function ($) {
+  $('#carrousel-links-rapidos').slick({
+    dots: false,
+    lazyLoad: 'ondemand',
+    appendArrows: $('.custom-arrows'), // Especifica o elemento para as setas
+    infinite: true,
+    speed: 300,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+    ]
+  });
+});
