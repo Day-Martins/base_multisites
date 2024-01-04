465b73dae47621930435a4dc57327e8d0df73658
Gabriel Carneiro <biel.dev@outlook.com>
2024-01-04 15:54:28 -0300

    hotfix(sass): correção no compilador

--- sheets.js
+++ sheets.js
@@ -15,69 +15,69 @@ const sheets = [
     // THEME BAGOV.BASE
     // NOTICIAS
     {
-        scss_src: theme_bagov_base_src + 'noticias/*.scss',
+        scss_src: theme_bagov_base_src + 'noticias/noticia.scss',
         css_name: 'noticia',
         css_dest: theme_bagov_base_dest + 'noticias/'
     },
     {
-        scss_src: theme_bagov_base_src + 'noticias/*.scss',
+        scss_src: theme_bagov_base_src + 'noticias/noticia-paginacao.scss',
         css_name: 'noticia-paginacao',
         css_dest: theme_bagov_base_dest + 'noticias/'
     },
     {
-        scss_src: theme_bagov_base_src + 'noticias/*.scss',
+        scss_src: theme_bagov_base_src + 'noticias/noticia-anexos.scss',
         css_name: 'noticia-anexos',
         css_dest: theme_bagov_base_dest + 'noticias/'
     },
     // MODULE BAGOV.BASE BLOCKS
     // CONTRASTE
     {
-        scss_src: module_bagov_base_blocks_src + 'contraste/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'contraste/contraste.scss',
         css_name: 'contraste',
         css_dest: module_bagov_base_blocks_dest + 'contraste/'
     },
     // EVENTOS
     {
-        scss_src: module_bagov_base_blocks_src + 'eventos/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'eventos/eventos.scss',
         css_name: 'eventos',
         css_dest: module_bagov_base_blocks_dest + 'eventos/'
     },
     // LOGO ESTADO
     {
-        scss_src: module_bagov_base_blocks_src + 'logo_estado/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'logo_estado/logo-estado.scss',
         css_name: 'logo-estado',
         css_dest: module_bagov_base_blocks_dest + 'logo_estado/'
     },
     // NOTICIAS
     {
-        scss_src: module_bagov_base_blocks_src + 'noticias/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'noticias/noticias.scss',
         css_name: 'noticias',
         css_dest: module_bagov_base_blocks_dest + 'noticias/'
     },
     {
-        scss_src: module_bagov_base_blocks_src + 'noticias/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'noticias/noticias-home.scss',
         css_name: 'noticias-home',
         css_dest: module_bagov_base_blocks_dest + 'noticias/'
     },
     {
-        scss_src: module_bagov_base_blocks_src + 'noticias/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'noticias/noticias-home-secundarias.scss',
         css_name: 'noticias-home-secundarias',
         css_dest: module_bagov_base_blocks_dest + 'noticias/'
     },
     {
-        scss_src: module_bagov_base_blocks_src + 'noticias/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'noticias/noticias-relacionadas.scss',
         css_name: 'noticias-relacionadas',
         css_dest: module_bagov_base_blocks_dest + 'noticias/'
     },
     // REDES SOCIAIS
     {
-        scss_src: module_bagov_base_blocks_src + 'redes_sociais/*.scss',
+        scss_src: module_bagov_base_blocks_src + 'redes_sociais/redes-sociais.scss',
         css_name: 'redes-sociais',
         css_dest: module_bagov_base_blocks_dest + 'redes_sociais/'
     },
     // Text resize
     {
-      scss_src: module_text_resize_src + 'text_resize/*.scss',
+      scss_src: module_text_resize_src + 'text_resize/text_resize.scss',
       css_name: 'text-resize',
       css_dest: module_text_resize_dest + 'text_resize/'
     },
@@ -100,11 +100,11 @@ const sheets = [
       css_dest: module_bagov_base_blocks_dest + 'publicacao/'
     },
      // Youtube
-		{
-			scss_src: module_bagov_base_blocks_src + 'youtube/*.scss',
-			css_name: 'youtube-carousel',
-			css_dest: module_bagov_base_blocks_dest + 'youtube/'
-		},
+     {
+        scss_src: module_bagov_base_blocks_src + 'youtube/*.scss',
+        css_name: 'youtube-carousel',
+        css_dest: module_bagov_base_blocks_dest + 'youtube/'
+      },
     // Youtube Player
     {
         scss_src: module_bagov_base_blocks_src + 'youtube-player/*.scss',
