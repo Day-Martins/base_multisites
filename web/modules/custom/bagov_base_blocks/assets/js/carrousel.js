1d9c80636a480672b6a71418344f49cfdde6691e
Murilo Montino <murilomontinojr@hotmail.com>
2024-01-05 14:28:15 -0300

    fix(carrousel-links-rapidos): remoção de classe que fazia ocupar 100% cada linha

--- web/modules/custom/bagov_base_blocks/assets/js/carrousel.js
+++ web/modules/custom/bagov_base_blocks/assets/js/carrousel.js
@@ -55,20 +55,6 @@ jQuery(document).ready(function ($) {
     slidesToScroll: 4,
     responsive: [
       {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-      {
         breakpoint: 768,
         settings: {
           slidesToShow: 3,
